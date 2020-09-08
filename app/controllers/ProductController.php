50ef14a39506a82fd67badb6272d13beffaad505
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-08 15:02:51 +0300

    Version 3.0. Card Product. Related products

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -5,14 +5,14 @@ class ProductController extends AppController{
     public function viewAction() {
        $alias=$this->route['alias'];
        $product=\R::findOne('product',"alias=? AND status='1'",[$alias]);
-//       debug($product);
         if (!$product) {
             throw new \Exception("Продукт $alias не найден в БД", 404);
         }
-        
+        $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id=related_product.related_id WHERE related_product.product_id=?", [$product->id]);
+//       debug($related);
         
         
         $this->setMeta($product->title,$product->description,$product->keywords);
-        $this->set(compact('product'));
+        $this->set(compact('product', 'related'));
     }
 }
