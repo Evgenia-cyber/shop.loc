b8f6c3120c792d69c8a09d1819601f2db5a8c71b
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-09 13:01:47 +0300

    Version 5.0. CardProduct. Added last three Resently Viewed Poducts

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -10,13 +10,21 @@ class ProductController extends AppController {
         if (!$product) {
             throw new \Exception("Продукт $alias не найден в БД", 404);
         }
-        $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id=related_product.related_id WHERE related_product.product_id=?", [$product->id]);
-        $gallery = \R::findAll('gallery', 'product_id=?', [$product->id]);
-//       debug($gallery);
 
+        $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id=related_product.related_id WHERE related_product.product_id=?", [$product->id]);
+
+        $product_model = new \app\models\Product;
+        $product_model->setRecentlyViewed($product->id);
+        $recently_viewed_from_cookie = $product_model->getRecentlyViewed();
+        $recently_viewed = null;
+        if ($recently_viewed_from_cookie) {
+            $recently_viewed = \R::find('product', 'id IN (' . \R::genSlots($recently_viewed_from_cookie) . ') LIMIT 3', $recently_viewed_from_cookie);
+        }
+
+        $gallery = \R::findAll('gallery', 'product_id=?', [$product->id]);
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related', 'gallery'));
+        $this->set(compact('product', 'related', 'gallery', 'recently_viewed'));
     }
 
 }
