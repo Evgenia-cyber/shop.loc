dca3360cf7ea6150bcca62fc3bb25045c3ef8f90
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-16 15:03:51 +0300

    Version 9.0. Search. Plagin typeahead.js version 0.11.1. Make styles in public/css/style.css.

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -89,10 +89,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                     </div>
                     <div class="col-md-3 header-right">
                         <div class="search-bar">
-                            <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {
-                                        this.value = 'Search';
-                                    }">
-                            <input type="submit" value="">
+                            <form action="search" method="get" autocomplete="off">
+                                <input type="text" class="typeahead" id="typeahead" name="s" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {
+                                            this.value = 'Search';
+                                        }">
+                                <!--<input type="text" class="typeahead" id="typeahead" name="s">-->
+                                <input type="submit" value="">
+                            </form>
                         </div>
                     </div>
                     <div class="clearfix"> </div>
@@ -101,7 +104,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         </div>
         <!--bottom-header-->
         <div class="content">
-            <?php debug($_SESSION)?>
+            <?php // debug($_SESSION)?>
             <?= $content; ?>
         </div>
 
@@ -201,6 +204,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         </script>
         <script src="js/jquery-1.11.0.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <script src="js/typeahead.bundle.js"></script>
         <!--dropdown-->
         <script src="js/jquery.easydropdown.js"></script>
         <!--Slider-Starts-Here-->
