652942dec38d85570ff463856cce8eafe01a673c
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-19 10:45:59 +0300

    Version 11.0.Registration. Create Model, Controller, View. Get data from form in array in Model

--- vendor/shop/core/base/Model.php
+++ vendor/shop/core/base/Model.php
@@ -8,12 +8,20 @@ use shop\Db;
  * @author Evgeniya
  */
 abstract class Model {
-    
+
     public $attributes = [];
     public $errors = [];
     public $rules = [];
-    
+
     public function __construct() {
         Db::instance();
+    }
+
+     public function load($data){
+        foreach($this->attributes as $name => $value){
+            if(isset($data[$name])){
+                $this->attributes[$name] = $data[$name];
+            }
+        }
     }
 }
