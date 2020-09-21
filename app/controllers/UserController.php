dc3a6e495955d14863507aab42938d346fbf913b
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-21 16:13:10 +0300

    Version 11.4.Registration. Autorization.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -10,22 +10,46 @@ public function signupAction(){
             $user = new User();
             $data = $_POST;
             $user->load($data);
-
             if(!$user->validate($data) || !$user->checkUnique()){
                 $user->getErrors();
+                $_SESSION['form_data'] = $data;
             }else{
                 $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
-                  if($user->save('user')){
+//                    debug($data);
+                  if($id=$user->save('user')){
                     $_SESSION['success'] = 'Вы успешно зарегистрировались';
+                    foreach($data as $k => $v){
+                    if($k != 'password'){ $_SESSION['user'][$k] = $v;}
+                    }
+                     $_SESSION['user']['id'] = $id;
+                     redirect(PATH);
                 }else{
                     $_SESSION['error'] = 'Ошибка!';
+                    redirect();
                 }
             }
-            redirect();
 
     }
     $this->setMeta('Регистрация');
 }
- public function loginAction(){}
-  public function logoutAction(){}
+
+   public function loginAction(){
+        if(!empty($_POST)){
+            $user = new User();
+            if($user->login()){
+                $_SESSION['success'] = 'Вы успешно авторизовались';
+                redirect(PATH);
+            }else{
+                $_SESSION['error'] = 'Вы ввели неправильный логин или пароль!';
+            }
+            redirect();
+        }
+        $this->setMeta('Вход');
+    }
+
+   public function logoutAction(){
+        if(isset($_SESSION['user'])) unset($_SESSION['user']);
+        redirect();
+    }
+
 }
