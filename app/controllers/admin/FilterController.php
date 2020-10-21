aefd14c257d1e6490256d24fe2c2532886de22cd
Evgenia Kurineva <kurineva85@mail.ru>
2020-10-21 14:33:08 +0300

    Version 18.5.2.Admin part of CMS. Filter list. Edditing group of filter. Edditing filter. Deleting filter.

--- app/controllers/admin/FilterController.php
+++ app/controllers/admin/FilterController.php
@@ -42,6 +42,27 @@ class FilterController extends AppController {
         $this->setMeta('Новая группа фильтров');
     }
 
+    public function groupEditAction() {
+        if (!empty($_POST)) {
+            $id= $this->getRequestID(FALSE);
+            $group = new FilterGroup();
+            $data = $_POST;
+            $group->load($data);
+            if (!$group->validate($data)) {
+                $group->getErrors();
+                redirect();
+            }
+            if ($group->update('attribute_group', $id)) {
+                $_SESSION['success'] = 'Изменения сохранены';
+                redirect();
+            }
+        }
+        $id = $this->getRequestID();
+        $group = \R::load('attribute_group', $id);
+        $this->setMeta("Редактирование группы фильтров {$group->title}");
+        $this->set(compact('group'));
+    }
+
     public function attributeAction() {
         $attrs = \R::getAssoc("SELECT attribute_value.*, attribute_group.title FROM attribute_value JOIN attribute_group ON attribute_group.id=attribute_value.attr_group_id");
         $this->setMeta("Фильтры");
@@ -62,9 +83,39 @@ class FilterController extends AppController {
                 redirect();
             }
         }
-        $group=\R::findAll('attribute_group');
+        $group = \R::findAll('attribute_group');
         $this->setMeta('Новый фильтр');
         $this->set(compact('group'));
+    }
+
+       public function attributeEditAction() {
+        if (!empty($_POST)) {
+            $id= $this->getRequestID(FALSE);
+            $attr = new FilterAttr();
+            $data = $_POST;
+            $attr->load($data);
+            if (!$attr->validate($data)) {
+                $attr->getErrors();
+                redirect();
+            }
+            if ($attr->update('attribute_value', $id)) {
+                $_SESSION['success'] = 'Изменения сохранены';
+                redirect();
+            }
+        }
+        $id = $this->getRequestID();
+        $attr = \R::load('attribute_value', $id);
+        $group = \R::findAll('attribute_group');
+        $this->setMeta("Редактирование фильтра {$attr->value}");
+        $this->set(compact('attr','group'));
+    }
+
+        public function attributeDeleteAction() {
+        $id = $this->getRequestID();
+        \R::exec('DELETE FROM attribute_product WHERE attr_id=?', [$id]);
+        \R::exec('DELETE FROM attribute_value WHERE id=?', [$id]);
+        $_SESSION['success'] = "Фильтр удален";
+        redirect();
     }
 
 }
